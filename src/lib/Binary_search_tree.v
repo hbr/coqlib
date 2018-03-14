0422ef40c3bc5a26ff43093b73f768625b00f3e2
hbr <helmut.brandl@gmx.net>
2018-03-13 19:26:17 -0600

    Added function for element search within a sorted tree
    	modified:   src/lib/Binary_search_tree.v

--- src/lib/Binary_search_tree.v
+++ src/lib/Binary_search_tree.v
@@ -3,12 +3,19 @@ Require Binary_tree.
 
 Set Implicit Arguments.
 
+Set Warnings "-extraction-opaque-accessed".
+Set Warnings "-extraction-inside-module".
+Unset Elimination Schemes.
+
+
 Module Make (S0:SORTABLE) (E:ANY).
   Module S := Sortable_plus S0.
   Import S.Notations.
 
   Module BT := Binary_tree.Make S E.
   Import BT.
+
+
 
   (*====================================*)
   (** * Sorted Binary Tree              *)
@@ -106,6 +113,35 @@ Module Make (S0:SORTABLE) (E:ANY).
         use_node0 b eq_refl.
 
 
+    Theorem use_node_P0:
+      forall (A:Prop) e x u v,
+        P (Tree.Node e u x v) ->
+        (forall lo hi,
+            lo <= x -> x <= hi -> R lo u x -> R x v hi -> A) ->
+        A.
+    Proof
+      fun A e x u v p =>
+        match p with
+        | make s =>
+          use_node
+            s
+            (fun lox xhi s1 s2 f =>
+               f _ _ lox xhi s1 s2)
+        end.
+
+    Theorem use_node_P:
+      forall (A:Prop) e x u v,
+        P (Tree.Node e u x v) ->
+        (P u -> P v -> A) ->
+        A.
+    Proof
+      fun A e x u v p =>
+        use_node_P0
+          p
+          (fun lo hi lox xhi s1 s2 f =>
+             f (make s1) (make s2)).
+
+
     Theorem ignore_rotation:
       forall (t u: Tree.t),
         Rotation.R t u ->
@@ -162,13 +198,143 @@ Module Make (S0:SORTABLE) (E:ANY).
         ignore_rotation
           (Rotation.change_extra e (Rotation.reflexive t))
           b.
+
+    (** All elements are within the bounds. *)
+    Theorem use_elements_bounded:
+      forall (A:Prop) x lo t hi,
+        R lo t hi ->
+        Domain.R t x ->
+        (lo <= x -> x <= hi -> A) ->
+        A.
+    Proof
+      fix f A x lo t hi s :=
+      match s with
+      | leaf le =>
+        fun has =>
+          ex_falso ((@Domain.leaf_empty x) has)
+      | node e s1 s2 =>
+        fun dom =>
+          let p := f A x _ _ _ s1 in
+          let q := f A x _ _ _ s2 in
+          Domain.use_node
+            dom
+            (fun eq g =>
+               g (Equal.use_bwd eq (fun z => _ <= z) (less_equal_lo_hi s1))
+                 (Equal.use_bwd eq (fun z => z <= _) (less_equal_lo_hi s2)))
+            (fun dom1 g =>
+               (f A x _ _ _ s1  dom1)
+                 (fun lox xy =>
+                    g lox (S.transitive xy (less_equal_lo_hi s2)))
+               )
+            (fun dom2 g =>
+               (f A x _ _ _ s2 dom2)
+                 (fun yx xhi =>
+                    g (S.transitive (less_equal_lo_hi s1) yx)
+                      xhi))
+      end.
   End Sorted.
 
 
 
 
+  (*====================================*)
+  (** * Searching Elements              *)
+  (*====================================*)
+  Module Search.
+    (** [search x t s] searches for an element equivalent to [x] in the sorted
+    tree [t] where [s] is a proof that [t] is sorted and returns either an
+    element satisfying the requirement or a proof that no element in the tree
+    satisfies the requirement.*)
+    Fact search:
+      forall (x:S.t) (t:Tree.t),
+        Sorted.P t ->
+        {e:S.t | Domain.R t e /\ S.Equivalent e x} +
+        {forall e, Domain.R t e -> ~ S.Equivalent e x}.
+    Proof
+      fix f x t :=
+      match t with
+      | Tree.Leaf =>
+        fun s  =>
+          inright ( fun e dom => ex_falso (Domain.leaf_empty dom) )
+      | Tree.Node _ u y v =>
+        match S.compare3_strict x y with
 
+        | Relation.LT_strict xy not_yx=>
 
+          fun s =>
+            match f x u (Sorted.use_node_P s (fun s1 _ => s1)) with
+            | inleft p =>
+              match p with
+              | exist _ z (conj dom1 eqv) =>
+                inleft (exist _ z (conj (Domain.left _ _ _ dom1) eqv))
+              end
+            | inright q =>
+              inright
+                (fun e dom =>
+                   Domain.use_node
+                     dom
+                     (fun eq_e_y eqv_e_x =>
+                        match eqv_e_x with
+                        | conj ex xe =>
+                            not_yx (Equal.use eq_e_y (fun e => e <= x) ex)
+                        end)
+                     (q e)
+                     (Sorted.use_node_P0
+                        s
+                        (fun lo hi lox xhi s1 s2 domv =>
+                           (Sorted.use_elements_bounded
+                              s2 domv
+                              (fun y_e e_hi eqv =>
+                                 match eqv with
+                                 | conj e_x x_e =>
+                                   not_yx (S.transitive y_e e_x)
+                                 end)
+                        ))
+                     )
+                )
+            end
+
+        | Relation.EQV_strict xy yx =>
+
+          fun s =>
+            inleft (exist _ y (conj (Domain.root _ u y v) (conj yx xy)))
+
+        | Relation.GT_strict yx not_xy =>
+
+          fun s =>
+            match f x v (Sorted.use_node_P s (fun _ s2 => s2)) with
+            | inleft p =>
+              match p with
+              | exist _ z (conj dom2 eqv) =>
+                inleft (exist _ z (conj (Domain.right _ _ _ dom2) eqv))
+              end
+            | inright q =>
+              inright
+                (fun e dom =>
+                   Domain.use_node
+                     dom
+                     (fun eq_e_y eqv_e_x =>
+                        match eqv_e_x with
+                        | conj ex xe =>
+                          not_xy (Equal.use eq_e_y (fun e => x <= e) xe)
+                        end)
+                     (Sorted.use_node_P0
+                        s
+                        (fun lo hi lox xhi s1 s2 domu =>
+                           (Sorted.use_elements_bounded
+                              s1 domu
+                              (fun loe ey eqv =>
+                                 match eqv with
+                                 | conj ex xe =>
+                                   not_xy (S.transitive xe ey)
+                                 end)
+                              )))
+                     (q e)
+                )
+            end
+        end
+      end.
+  End Search.
 
 
   (*====================================*)
